af36de1492bb14d7a60ff354fcf0d97dbebac4b6
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-13 18:58:51 +0530

    created NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,14 +5,34 @@ import {NavLink} from "react-router-dom";
 function NavBar() {
     return (
         <div className="NavBar">
-            <div className="profile">
-                <img src={avatar} alt=""/>
-            </div>
-            <div className="nav">
+            <nav className="nav">
+                <div className="profile">
+                    <img src={avatar} alt=""/>
+                </div>
                 <ul className="nav-items">
-                    <li className="navitem">
-                        <NavLink to="/" exact>
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
                             Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/portfolios" exact activeClassName="active">
+                            Portfolios
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/blogs" exact activeClassName="active">
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contact" exact activeClassName="active">
+                            Contact
                         </NavLink>
                     </li>
                 </ul>
@@ -21,8 +41,7 @@ function NavBar() {
                         @2021 Lorem Ipsum
                     </p>
                 </footer>
-            </div>
-            
+            </nav>
         </div>
     )
 }
