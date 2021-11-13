f26d7dc34e264182ae364c9577f19bb8c1999c39
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-13 14:41:41 +0530

    created NavBar, added profile to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.scss'
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
      
-        <div className="sidebar"></div>
+        <div className="sidebar">
+          <NavBar/>
+        </div>
         <div className="main-content"></div>
     </div>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import avatar from '../images/avatar.png'
+import React from 'react';
+import avatar from '../images/avatar.png';
+import {NavLink} from "react-router-dom";
 
 function NavBar() {
     return (
         <div className="NavBar">
             <div className="profile">
                 <img src={avatar} alt=""/>
+            </div>
+            <div className="nav">
+                <ul className="nav-items">
+                    <li className="navitem">
+                        <NavLink to="/" exact>
+                            Home
+                        </NavLink>
+                    </li>
+                </ul>
+                <footer className="footer">
+                    <p>
+                        @2021 Lorem Ipsum
+                    </p>
+                </footer>
             </div>
             
         </div>
