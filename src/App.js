b6e8d9fb27f65469bb6e9130a8a237191e5a2660
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-15 13:04:29 +0530

    created Portfolios Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './Components/NavBar';
 import HomePage from './Pages/HomePage';
 import { Routes ,Route } from 'react-router-dom';
 import AboutPage from './Pages/AboutPage';
+import PortfoliosPage from './Pages/PortfoliosPage';
+
 
 function App() {
   return (
@@ -14,12 +16,10 @@ function App() {
         <div className="main-content">
           <div className="content">
             <Routes>
-              <Route path="/" element={<HomePage/>} >
-              </Route>
-              <Route path="/about"  element={<AboutPage />}>
-                
-              </Route>
-           </Routes>
+              <Route path="/" element={<HomePage/>} />
+              <Route path="/about"  element={<AboutPage />}/>
+              <Route path="/portfolios"  element={<PortfoliosPage/>}/>
+            </Routes>
           </div>
         </div>
     </div>
---
+++ src/Pages/PortfoliosPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function PortfoliosPage() {
+    return (
+        <div className="AboutPage">
+           <h2>Portfolios Page</h2>
+        </div>
+    )
+}
+export default  PortfoliosPage;
