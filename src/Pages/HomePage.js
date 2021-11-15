802db855a4343b0070728cb6bbfb11c9eaaedb30
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-15 12:18:31 +0530

    added fb, github and youtube icons to main content page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
 
 function HomePage() {
     return (
@@ -15,7 +20,15 @@ function HomePage() {
                     A sint ipsam earum nemo omnis et.
                 </p>
                 <div className="icons">
-                    
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon fb" />
+                    </Link>
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
+                    </Link>
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon yt" />
+                    </Link>
                 </div>
             </header>
         </div>
