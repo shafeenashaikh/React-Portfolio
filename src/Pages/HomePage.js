ebaee8790d6e579b9e7acc19259e8212caa84061
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-15 11:28:57 +0530

    created main content page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
-            <h2>Home Page</h2>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am
+                    <span>Lorem Ipsum.</span>
+                </h1>
+                <p className="h-sub-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Cupiditate facere, numquam vitae placeat consequatur corrupti officia 
+                    quibusdam. Blanditiis doloremque nemo ex facilis neque. 
+                    A sint ipsam earum nemo omnis et.
+                </p>
+                <div className="icons">
+                    
+                </div>
+            </header>
         </div>
     )
 }
