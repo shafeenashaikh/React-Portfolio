aff6e621a1fce2f4747ed0c2b920a1d1bb335e28
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 16:19:30 +0530

    added About details

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -9,35 +9,27 @@ function ImageSection() {
             </div>
             <div className="about-info">
                 <h4>I am<span> Lorem Ipsum</span></h4>
-                <p>
+                <p className="about-text">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Voluptatum necessitatibus perferendis numquam dolorem cumque facilis.
                     Desir per po la ta doso vi slone ve?
                 </p>
                 <div className="about-details">
-                    <div className="name-details">
+                    <div className="left-section">
                         <p>Full Name</p>
-                        <p>: Lorem Ipsum</p>
-                    </div>
-                    <div className="name-details">
                         <p>Age</p>
-                        <p>: 50</p>
-                    </div>
-                    <div className="name-details">
                         <p>Nationality</p>
-                        <p>: New Zealand</p>
-                    </div>
-                    <div className="name-details">
                         <p>Language</p>
-                        <p>: Spanish, French, English</p>
-                    </div>
-                    <div className="name-details">
                         <p>Address</p>
-                        <p>: 2 Beightbridge, BN7 GB9, United kingdom</p>
-                    </div>
-                    <div className="name-details">
                         <p>Countries</p>
-                        <p>: Spain, France</p>
+                    </div>
+                    <div className="right-section">
+                         <p>: Lorem Ipsum</p>
+                        <p>: 50</p>
+                        <p>: French</p>
+                        <p>: French, Spanish, English</p>
+                        <p>: 2 Beightbridge, BN7GB9,United kingdom</p>
+                        <p>: France, Spain, Germany</p>
                     </div>
                 </div>
             </div>
