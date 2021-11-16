25d4b83bb9e280cf09387dd8ff0efee7b815c83d
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 12:11:38 +0530

    imported ImageSection

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ImageSection from '../Components/ImageSection';
 import Tittle from '../Components/Tittle';
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Tittle title={'About Me'} span={'About Me'}/>
+            <ImageSection/>
         </div>
     )
 }
