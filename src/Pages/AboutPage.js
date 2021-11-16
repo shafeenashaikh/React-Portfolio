a6bcf22fc8125ea115ccc3053885dfa80c0746d7
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 17:12:40 +0530

    added skills section to the about page

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ImageSection from '../Components/ImageSection';
+import SkillsSection from '../Components/SkillsSection';
 import Tittle from '../Components/Tittle';
 
 function AboutPage() {
@@ -8,6 +9,7 @@ function AboutPage() {
             <Tittle title={'About Me'} span={'About Me'}/>
             <ImageSection/>
             <Tittle title={'My Skills'} span={'My Skills'}/>
+            <SkillsSection/>
         </div>
     )
 }
