99ea7e1f477d63100b3e561d62304f03b9237f9a
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 17:13:09 +0530

    added skills section to the about page

--- src/Components/SkillsSection.js
+++ src/Components/SkillsSection.js
@@ -3,7 +3,15 @@ import React from 'react'
 function SkillsSection() {
     return (
         <div className="SkillsSection">
-            
+            <div className="skills-container">
+                <h5 className="skill-title"> </h5>
+                <div className="skill-bar">
+                    <p className="skill-text"> </p>
+                    <div className="skill-progress">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
