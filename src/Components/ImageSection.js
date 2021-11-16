0f46a1e4db8688f9e91b86345529de961bde5944
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 12:07:48 +0530

    created ImageSection component

---
+++ src/Components/ImageSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import about from '../images/about.png'
+
+function ImageSection() {
+    return (
+        <div className="ImageSection">
+            <div className="img">
+                <img src={about} alt=""/>
+            </div>
+            <div className="about-info">
+                <h4>I am<span>Lorem Ipsum</span></h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Voluptatum necessitatibus perferendis numquam dolorem cumque facilis.
+                    Desir per po la ta doso vi slone ve?
+                </p>
+                <div className="about-details">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ImageSection;
