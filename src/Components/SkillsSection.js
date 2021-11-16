5478f356e02eadf1def6ef08060d853b124936ca
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 17:06:00 +0530

    created SkillsSection and added to the aboutPage

---
+++ src/Components/SkillsSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SkillsSection() {
+    return (
+        <div className="SkillsSection">
+            
+        </div>
+    )
+}
+export default SkillsSection;
