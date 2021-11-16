89d09230d8021d9eb28d6a3c1496c4fae613ec1a
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 10:43:34 +0530

    Created Contact Page

---
+++ src/Pages/ContactPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ContactPage() {
+    return (
+        <div className="ContactPage">
+            <h1>Contact Page</h1>
+        </div>
+    )
+}
+export default ContactPage;
