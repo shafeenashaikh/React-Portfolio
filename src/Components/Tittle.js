c014b16cc929decba93792b10381837e6f3a42f5
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-16 10:55:57 +0530

    created Tittle page

---
+++ src/Components/Tittle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tittle() {
+    return (
+        <div className="Tittle">
+            <h3>
+                <span></span>
+            </h3>
+        </div>
+    )
+}
+export default Tittle;
