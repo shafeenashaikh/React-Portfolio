1b0ab55fe2e9be1663ff760d146ed3bf576c8a57
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-17 12:28:34 +0530

    added more Skills to the SkillsSection

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -9,7 +9,17 @@ function AboutPage() {
             <Tittle title={'About Me'} span={'About Me'}/>
             <ImageSection/>
             <Tittle title={'My Skills'} span={'My Skills'}/>
-            <SkillsSection skill={'Javascript'} progress={'50%'} width={'50%'}/>
+            <div className="skills-container">
+                <SkillsSection skill={'Javascript'} progress={'50%'} width={'50%'}/>
+                <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
+                <SkillsSection skill={'React Js'} progress={'70%'} width={'70%'} />
+                <SkillsSection skill={'Node Js'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Python'} progress={'90%'} width={'90%'} />
+                <SkillsSection skill={'Lua'} progress={'40%'} width={'40%'} />
+                <SkillsSection skill={'Java'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Web Design'} progress={'25%'} width={'25%'} />
+                <SkillsSection skill={'UI/Ux Design'} progress={'76%'} width={'76%'} />
+            </div>
         </div>
     )
 }
