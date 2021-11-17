b03952f4df04c85b5aeb0bc7714f212a9bd4cfe1
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-17 13:08:23 +0530

    created servicesSection component

---
+++ src/Components/ServicesSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicesSection() {
+    return (
+        <div className="ServicesSection">
+            
+        </div>
+    )
+}
+
+export default ServicesSection;
