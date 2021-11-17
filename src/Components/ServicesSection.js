2f96d7c9dd8ae98ac8b39d6040ec0a8b37832867
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-17 16:07:30 +0530

    added image and main content to the service section

--- src/Components/ServicesSection.js
+++ src/Components/ServicesSection.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-function ServicesSection({image}) {
+function ServicesSection({image, title}) {
     return (
         <div className="ServicesSection">
             <div className="services">
+                <div className="service-content">
                 <img src={image} alt=""/>
-                <h5 className="s-title"> </h5>
+                <h5 className="s-title">{title}</h5>
                 <p className="s-text">
                     Lorem Ipsum dolor sit amet consecteture adipisicing elit.
                 </p>
+                </div>
             </div>
         </div>
     )
