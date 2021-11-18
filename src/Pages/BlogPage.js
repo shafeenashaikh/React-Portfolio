3f0b93c7c2a051301fdb3646bd4e48b09501d5ca
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-18 15:58:24 +0530

    added all the blogs with blog content  and images to the BlogPage

--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import AllBlogs from '../Components/allBlogs';
+import blogs from '../Components/allBlogs';
 
 function BlogPage() {
     return (
     <div className="BlogsPage">
-        
+        {
+            blogs.map((blog) =>{
+                return (
+                    <div className="blog" key={blog.id}>
+                        <div className="blog-content">
+                            <img src={blog.image} alt=""/>
+                            <a href="github.com" className="blog-link">
+
+                            </a>
+                        </div>
+                    </div>
+                )
+            })
+        }
     </div>
     )
 }
