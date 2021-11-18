345bda780025a878339e40e9841283b411da5cb2
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-18 19:42:59 +0530

    added different contact details like Phone,Email and Address

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-function ContactItem() {
+function ContactItem({icon, text1, text2, title}) {
     return (
-        <div>
-            
+        <div className="contactItem">
+            <img src={icon} alt=""/>
+            <div className="right-item">
+                <h6>{title}</h6>
+                <p>{text1}</p>
+                <p>{text2}</p>
+            </div>
         </div>
     )
 }
 
-export default ContactItem
+export default ContactItem;
--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import ContactItem from '../Components/ContactItem';
+import phone from '../images/phone.svg';
+import email from '../images/emailme.svg';
+import location from '../images/location.svg';
 
 function ContactPage() {
     return (
         <div className="ContactPage">
-            <h1>Contact Page</h1>
+            <div className="map-sect"></div>
+            <div className="contact-sect">
+                <ContactItem icon={phone} text1={'+19 7756118805'} text2={'+19 6782593214'} title={'Phone'}/>
+                <ContactItem icon={email} text1={'Diana@gmail.com'} text2={'John12@gmail.com'} title={'Email'}/>
+                <ContactItem icon={location} text1={'25 Paramount st, PN4 8H9 London'} text2={'United Kingdoms'} title={'Address'}/>
+            </div>
         </div>
     )
 }
