2c564aa4b4539bc59ec34f51caf3a4b15b675598
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-18 20:17:10 +0530

    Done styling of the ContactPage

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -3,11 +3,13 @@ import React from 'react'
 function ContactItem({icon, text1, text2, title}) {
     return (
         <div className="contactItem">
-            <img src={icon} alt=""/>
-            <div className="right-item">
-                <h6>{title}</h6>
-                <p>{text1}</p>
-                <p>{text2}</p>
+            <div className="contact">
+                <img src={icon} alt=""/>
+                <div className="right-item">
+                    <h6>{title}</h6>
+                    <p>{text1}</p>
+                    <p>{text2}</p>
+                </div>
             </div>
         </div>
     )
