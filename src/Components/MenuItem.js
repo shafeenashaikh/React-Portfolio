eaabb7bc6d2b53aa383c974dd557eba545609603
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-19 11:36:41 +0530

    added list of links and title to the portfoliospage

--- src/Components/MenuItem.js
+++ src/Components/MenuItem.js
@@ -9,7 +9,19 @@ function MenuItem({menuItem}) {
                         <div className="portfolio" key={item.id}>
                             <div className="image-data">
                                 <img src={item.image} alt=""/>
+                                <ul className="hover-items">
+                                    <li>
+                                    <a href={item.link1}> </a>
+                                    <a href={item.link2}> </a>
+                                    </li>
+                                </ul>
                             </div>
+                            <h5>
+                                {item.title}
+                            </h5>
+                            <p>
+                                Placeholder Paragraph
+                            </p>
                         </div>
                     )
                 })
