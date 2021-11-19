e1a0de7960a82bcce01eaab1a7fb418adb7b7686
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-19 11:07:33 +0530

    created Categories for portfolios page and imported in portfoliospage

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Categories from '../Components/Categories';
 import Tittle from '../Components/Tittle';
 
 function PortfoliosPage() {
     return (
         <div className="PortfoliosPage">
-            <Tittle title={'Portfolios'} span={'Portfolios'}/>
+            <div className="title">
+                <Tittle title={'Portfolios'} span={'Portfolios'}/>
+            </div>
+            <div className="portfolios">
+                <Categories/>
+            </div>
         </div>
+        
     )
 }
 export default  PortfoliosPage;
