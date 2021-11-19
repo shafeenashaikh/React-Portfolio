c01608966fcd560b1bd94755b1361f5b104ff610
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-19 10:59:28 +0530

    created allPortfolios details and imported required images

---
+++ src/Components/allPortfolios.js
@@ -0,0 +1,82 @@
+import port1 from '../images/portImages/maya-1.jpg';
+import port2 from '../images/portImages/port1.jpg';
+import port3 from '../images/portImages/port2.jpg';
+import port4 from '../images/portImages/port3.jpg';
+import port5 from '../images/portImages/port4.jpg';
+import port6 from '../images/portImages/port5.jpg';
+import port7 from '../images/portImages/port6.jpg';
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'Animation',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port1,
+        title: 'Autodesk Maya'
+    },
+    {
+        id: 2,
+        category: 'Python',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port2,
+        title: 'Pythin Web Scrapping'
+    },
+    {
+        id: 3,
+        category: 'Animation',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port3,
+        title: 'Autodesk Maya'
+    },
+    {
+        id: 4,
+        category: 'Javascript',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port4,
+        title: 'Vanilla Javascript Website'
+    },
+    {
+        id: 5,
+        category: 'React Js',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port6,
+        title: 'Getting Started With React'
+    },
+    {
+        id: 6,
+        category: 'React Js',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port7,
+        title: 'Vanilla Javascript Website'
+    },
+    {
+        id: 7,
+        category: 'Test Port',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: port4,
+        title: 'Vanilla Javascript Website'
+    },
+]
+
+export default portfolios;
